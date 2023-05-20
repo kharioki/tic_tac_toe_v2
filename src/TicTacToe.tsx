f215e047ee0afc7587db1148176ff81ac5896c1a
Tony Kharioki <antolisah@gmail.com>
2023-05-20 12:44:14 +0300

    using timeout and callbacks

--- src/TicTacToe.tsx
+++ src/TicTacToe.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { DIMENSIONS, PLAYER_X, PLAYER_O, SQUARE_DIMS, GAME_STATES } from './constants';
 import { getRandomInt, switchPlayer } from './utils';
@@ -20,35 +20,59 @@ export default function TicTacToe() {
     ai: null,
   });
   const [gameState, setGameState] = useState(GAME_STATES.notStarted);
+  const [nextMove, setNextMove] = useState<null | number>(null);
 
-  const move = (index: number, player: number | null) => {
-    if (player !== null) {
-      setGrid((grid) => {
-        const gridCopy = grid.concat();
-        gridCopy[index] = player;
-        return gridCopy;
-      });
-    }
-  };
+  // memoize the move function so it doesn't get re-created on every render unless the dependencies change
+  const move = useCallback(
+    (index: number, player: number | null) => {
+      if (player && gameState === GAME_STATES.inProgress) {
+        setGrid((grid) => {
+          const gridCopy = grid.concat();
+          gridCopy[index] = player;
+          return gridCopy;
+        });
+      }
+    }, 
+  [gameState]
+);
 
-  const aiMove = () => {
+  // we wrap the aiMove function in a useCallback hook so it doesn't get re-created on every render unless the dependencies change
+  const aiMove = useCallback(() => {
     let index = getRandomInt(0, grid.length - 1); // 0 - 8
     while (grid[index]) {
       index = getRandomInt(0, grid.length - 1);
     }
     move(index, players.ai);
-  };
+    setNextMove(players.human);
+  }, [move, grid, players]);
 
   const humanMove = (index: number) => {
-    if (!grid[index]) {
+    if (!grid[index] && nextMove === players.human) {
       move(index, players.human);
-      aiMove();
+      setNextMove(players.ai);
     }
   };
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    if (
+      nextMove !== null &&
+      nextMove === players.ai &&
+      gameState !== GAME_STATES.over
+    ) {
+      // Delay AI moves to make them seem natural
+      timeout = setTimeout(() => {
+        aiMove();
+      }, 500);
+    }
+    return () => timeout && clearTimeout(timeout);
+  }, [nextMove, aiMove, players.ai, gameState]);
 
   const choosePlayer = (option: number) => {
     setPlayers({ human: option, ai: switchPlayer(option) });
     setGameState(GAME_STATES.inProgress);
+    // Set the Player_X to make the first move
+    setNextMove(PLAYER_X);
   };
 
   return gameState === GAME_STATES.notStarted ? (
